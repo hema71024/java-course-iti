5bf74bc8922f4e57e3bf5bcdf86c74e2832b718c
hema71024 <56462439+hema71024@users.noreply.github.com>
2021-06-03 14:45:45 +0200

    Add files via upload

---
+++ assignment 6/src/main/java/tablesaw.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import tech.tablesaw.api.Table;
+public class tablesaw {
+        public static void main(String args[]) {
+        Table table;
+        try {
+            table = Table.read ().csv ("titanic.csv");
+            System.out.println("Structure of data :"+ table.structure());
+            System.out.println("Summary of data :"+ table.summary ());
+            System.out.println("number of missing values:"+ table.missingValueCounts());
+            System.out.println(" duplicate remove :"+ table.dropRowsWithMissingValues());
+            System.out.println("remove duplicate rows:"+ table.dropDuplicateRows());
+            System.out.println("Summary of data :"+ table.summary ());
+
+        } catch (IOException ex) {
+            Logger.getLogger(tablesaw.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
+}
